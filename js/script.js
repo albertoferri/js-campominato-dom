0221519304a072b686796de4a710ad0bf5ea4e87
albertoferri <falberto99@gmail.com>
2024-02-08 12:48:23 +0100

    aggiunta funzione per generare i numeri casuali

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function createGrid() {
     
     const selectElement = document.querySelector("#difficulty");
     console.log(selectElement.value);
-
+    
     if (selectElement.value == "easy"){
         cellNumber = 100;
     } else if (selectElement.value == "medium"){
@@ -42,7 +42,31 @@ function createGrid() {
         // cambio colore al click
         squareElement.addEventListener("click", function(){
             this.classList.toggle("active");
-            console.log("HAI SELEZIONATO LA CELLA: " + this.innerText)
+            console.log("HAI SELEZIONATO LA CELLA: " + this.innerText);
         });
     }
 }
+
+// PARTE 2
+// Il computer deve generare 16 numeri casuali e inserirli in un array
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. 
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+function randomNumber() {
+    // creo array vuoto
+    const numeriCasuali = [];
+    while (numeriCasuali.length < 16) {
+        const numeroCasuale = Math.floor(Math.random() * 100) + 1;
+        // controllo se il numero generato è già presente nell'array, se non lo è, lo pusho
+        if (!numeriCasuali.includes(numeroCasuale)) {
+            numeriCasuali.push(numeroCasuale);
+        }
+    }
+    return numeriCasuali;
+}
+
+// Esempio di utilizzo della funzione
+const randomElement = randomNumber();
+console.log(randomElement);
